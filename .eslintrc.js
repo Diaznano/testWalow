a1c30864fb3823dd3301cafe59d94ec9b44b11b8
Mariano Diaz <mariano.diaz@onetree.com>
2022-08-23 20:00:23 -0300

    initialize project and git

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    extends: [
+        'standard',
+        'prettier',
+        'prettier/flowtype',
+        'prettier/react',
+        'prettier/standard',
+    ],
+    plugins: ['react', 'react-native', 'flowtype', 'prettier', 'standard'],
+}
